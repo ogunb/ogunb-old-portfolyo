810eff77998c221509bbb93164fbaab3c3c4e70f
Ogunb <babacanogun@gmail.com>
2018-08-20 19:41:05 +0300

    Added scroll transitions for navigation.

--- src/app/app.js
+++ src/app/app.js
@@ -30,29 +30,48 @@ function loop(){
 }
 loop();
 
-//----------CardIMG Reveal & bg-changer----------//
+//----------Scroll Effects----------//
 
 window.addEventListener("scroll", () => {
     let cardItem = Array.from(document.querySelectorAll('.cardimg__item'))
     let body = document.querySelector('body')
     let contact = document.querySelector('.contact')
-    let logo = document.querySelector('.logo svg .c')
+    let logoInner = document.querySelector('.logo svg .c')
+    let logoOuter = document.querySelector('.logo svg .d')
+    let links = Array.from(document.querySelectorAll('.nav ul li a'))
 
     if(window.pageYOffset * 3.1 > canvas.height){
         for(let i = 0; i < cardItem.length; i++){
             cardItem[i].classList.add('cardimg--reveal')
         }
     }
-    if(window.pageYOffset + 200 > canvas.height){
-        body.style.backgroundColor = "var(--bookopus)"
-        logo.style.fill = "var(--light)"
-    } else{
-        body.style.backgroundColor = "var(--light)"
-        logo.style.fill = "var(--cta)"
+    function checkWorks(){
+        if(window.pageYOffset + 400 > canvas.height){
+            body.style.backgroundColor = "var(--bookopus)"
+            logoInner.style.fill = "var(--light)"
+            logoOuter.style.stroke = "var(--dark)"
+            for(let i = 0; i < links.length; i++){
+                links[i].style.color = "var(--dark)"
+            }
+        }    else{ /* Default Values */
+            body.style.backgroundColor = "var(--light)"
+            logoInner.style.fill = "var(--cta)"
+            logoOuter.style.stroke = "var(--dark)"
+            for(let i = 0; i < links.length; i++){
+                links[i].style.color = "var(--dark)"
+            }
+        }
     }
+    checkWorks();
     if(window.pageYOffset + contact.scrollHeight - 450  > canvas.height +  contact.scrollHeight ){
-        body.style.backgroundColor = "var(--light)"
-        logo.style.fill = "var(--cta)"
+        body.style.backgroundColor = "var(--dark)"
+        logoInner.style.fill = "var(--cta)"
+        logoOuter.style.stroke = "var(--light)"
+        for(let i = 0; i < links.length; i++){
+            links[i].style.color = "var(--light)"
+        }
+    }else if (window.pageYOffset + 200 > canvas.height || window.pageYOffset + contact.scrollHeight - 900  < canvas.height +  contact.scrollHeight){
+        checkWorks();
     }
 })
 
