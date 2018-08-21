cc7670e8eac816189f0b155888d312ce385fa73e
Ogunb <babacanogun@gmail.com>
2018-08-21 13:51:34 +0300

    Disabled noise bg.

--- src/app/app.js
+++ src/app/app.js
@@ -1,69 +1,69 @@
 //----------Noise-Background----------//
 let canvas = document.querySelector('.noise');
-let ctx = canvas.getContext('2d');
+// let ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
 window.addEventListener('resize', () => {canvas.width = window.innerWidth; canvas.height = window.innerHeight;});
 
-(function() {
-    var buffercanvas = document.createElement('canvas');
-    var bufferctx = buffercanvas.getContext('2d');
-    var WIDTH = 100;
-    var HEIGHT = 100;
-    buffercanvas.width = WIDTH;
-    buffercanvas.height = HEIGHT;
-    buffercanvas.fillStyle = '#000';
+// (function() {
+//     var buffercanvas = document.createElement('canvas');
+//     var bufferctx = buffercanvas.getContext('2d');
+//     var WIDTH = 100;
+//     var HEIGHT = 100;
+//     buffercanvas.width = WIDTH;
+//     buffercanvas.height = HEIGHT;
+//     buffercanvas.fillStyle = '#000';
   
-    function rand(num) {
-      return Math.floor(Math.random() * num);
-    }
+//     function rand(num) {
+//       return Math.floor(Math.random() * num);
+//     }
   
-    function tvstatic(canvas, ctx, scale) {
-      scale = scale || 1;
-      var h = canvas.height;
-      var w = canvas.width;
+//     function tvstatic(canvas, ctx, scale) {
+//       scale = scale || 1;
+//       var h = canvas.height;
+//       var w = canvas.width;
   
-      bufferctx.clearRect(0, 0, WIDTH, HEIGHT);
-      // draw the static on the buffer canvas
-      for (var x = 0; x < WIDTH; x+=scale) {
-        for (var y = 0; y < HEIGHT; y+=scale) {
-          if (Math.round(Math.random()))
-            bufferctx.fillRect(x, y, scale, scale);
-        }
-      }
+//       bufferctx.clearRect(0, 0, WIDTH, HEIGHT);
+//       // draw the static on the buffer canvas
+//       for (var x = 0; x < WIDTH; x+=scale) {
+//         for (var y = 0; y < HEIGHT; y+=scale) {
+//           if (Math.round(Math.random()))
+//             bufferctx.fillRect(x, y, scale, scale);
+//         }
+//       }
   
-      // repeat it onto the real canvas
-      for (var x = 0; x < canvas.width; x += WIDTH) {
-        for (var y = 0; y < canvas.height; y += HEIGHT) {
-          ctx.drawImage(buffercanvas, x, y);
-        }
-      }
+//       // repeat it onto the real canvas
+//       for (var x = 0; x < canvas.width; x += WIDTH) {
+//         for (var y = 0; y < canvas.height; y += HEIGHT) {
+//           ctx.drawImage(buffercanvas, x, y);
+//         }
+//       }
   
-      // draw some horizontal lines on the real canvas
-      for (var y = rand(10); y < canvas.height; y += rand(10)) {
-        ctx.fillRect(0, y, canvas.width, rand(3));
-      }
+//       // draw some horizontal lines on the real canvas
+//       for (var y = rand(10); y < canvas.height; y += rand(10)) {
+//         ctx.fillRect(0, y, canvas.width, rand(3));
+//       }
   
-    }
+//     }
   
-    window.tvstatic = tvstatic;
-  })();
-  window.addEventListener('load', load, false);
-		function load() {
-			canvas = document.querySelector('.noise');
-			ctx = canvas.getContext('2d');
-			ctx.fillStyle = '#f7f7f';
+//     window.tvstatic = tvstatic;
+//   })();
+//   window.addEventListener('load', load, false);
+// 		function load() {
+// 			canvas = document.querySelector('.noise');
+// 			ctx = canvas.getContext('2d');
+// 			ctx.fillStyle = '#f7f7f';
 
-			requestAnimationFrame(render);
-		}
+// 			requestAnimationFrame(render);
+// 		}
 
-		function render() {
-			requestAnimationFrame(render);
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
+// 		function render() {
+// 			requestAnimationFrame(render);
+// 			ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-			tvstatic(canvas, ctx, 2);
-		}
+// 			tvstatic(canvas, ctx, 2);
+// 		}
 
 //----------Scroll Effects----------//
 
