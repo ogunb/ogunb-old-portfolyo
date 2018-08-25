38f962e72f1f87c191bf3d2f8744dafa6d05120a
Ogunb <babacanogun@gmail.com>
2018-08-26 00:51:02 +0300

    Did some cleaning in js variables.

--- src/app/app.js
+++ src/app/app.js
@@ -1,71 +1,78 @@
 //----------Noise-Background----------//
-let canvas = document.querySelector('.noise');
-let ctx = canvas.getContext('2d');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
+if(document.querySelector('canvas')){
+    let canvas = document.querySelector('.noise');
+    let ctx = canvas.getContext('2d');
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
 
-window.addEventListener('resize', () => {canvas.width = window.innerWidth; canvas.height = window.innerHeight;});
+    window.addEventListener('resize', () => {canvas.width = window.innerWidth; canvas.height = window.innerHeight;});
 
-(function() {
-    var buffercanvas = document.createElement('canvas');
-    var bufferctx = buffercanvas.getContext('2d');
-    var WIDTH = 100;
-    var HEIGHT = 100;
-    buffercanvas.width = WIDTH;
-    buffercanvas.height = HEIGHT;
-    buffercanvas.fillStyle = '#000';
-  
-    function rand(num) {
-      return Math.floor(Math.random() * num);
-    }
-  
-    function tvstatic(canvas, ctx, scale) {
-      scale = scale || 1;
-      var h = canvas.height;
-      var w = canvas.width;
-  
-      bufferctx.clearRect(0, 0, WIDTH, HEIGHT);
-      // draw the static on the buffer canvas
-      for (var x = 0; x < WIDTH; x+=scale) {
-        for (var y = 0; y < HEIGHT; y+=scale) {
-          if (Math.round(Math.random()))
-            bufferctx.fillRect(x, y, scale, scale);
+    (function() {
+        var buffercanvas = document.createElement('canvas');
+        var bufferctx = buffercanvas.getContext('2d');
+        var WIDTH = 100;
+        var HEIGHT = 100;
+        buffercanvas.width = WIDTH;
+        buffercanvas.height = HEIGHT;
+        buffercanvas.fillStyle = '#000';
+    
+        function rand(num) {
+        return Math.floor(Math.random() * num);
         }
-      }
-  
-      // repeat it onto the real canvas
-      for (var x = 0; x < canvas.width; x += WIDTH) {
-        for (var y = 0; y < canvas.height; y += HEIGHT) {
-          ctx.drawImage(buffercanvas, x, y);
+    
+        function tvstatic(canvas, ctx, scale) {
+        scale = scale || 1;
+        var h = canvas.height;
+        var w = canvas.width;
+    
+        bufferctx.clearRect(0, 0, WIDTH, HEIGHT);
+        // draw the static on the buffer canvas
+        for (var x = 0; x < WIDTH; x+=scale) {
+            for (var y = 0; y < HEIGHT; y+=scale) {
+            if (Math.round(Math.random()))
+                bufferctx.fillRect(x, y, scale, scale);
+            }
         }
-      }
-  
-      // draw some horizontal lines on the real canvas
-      for (var y = rand(10); y < canvas.height; y += rand(10)) {
-        ctx.fillRect(0, y, canvas.width, rand(3));
-      }
-  
+    
+        // repeat it onto the real canvas
+        for (var x = 0; x < canvas.width; x += WIDTH) {
+            for (var y = 0; y < canvas.height; y += HEIGHT) {
+            ctx.drawImage(buffercanvas, x, y);
+            }
+        }
+    
+        // draw some horizontal lines on the real canvas
+        for (var y = rand(10); y < canvas.height; y += rand(10)) {
+            ctx.fillRect(0, y, canvas.width, rand(3));
+        }
+    
+        }
+    
+        window.tvstatic = tvstatic;
+    })();
+    window.addEventListener('load', load, false);
+    function load() {
+        canvas = document.querySelector('.noise');
+        ctx = canvas.getContext('2d');
+        ctx.fillStyle = '#f7f7f';
+
+        requestAnimationFrame(render);
     }
-  
-    window.tvstatic = tvstatic;
-  })();
-  window.addEventListener('load', load, false);
-		function load() {
-			canvas = document.querySelector('.noise');
-			ctx = canvas.getContext('2d');
-			ctx.fillStyle = '#f7f7f';
 
-			requestAnimationFrame(render);
-		}
+    function render() {
+        requestAnimationFrame(render);
+        ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-		function render() {
-			requestAnimationFrame(render);
-			ctx.clearRect(0, 0, canvas.width, canvas.height);
-
-			tvstatic(canvas, ctx, 2);
-		}
+        tvstatic(canvas, ctx, 2);
+    }
+}
 
 //----------Scroll Effects----------//
+let landingTop = document.querySelector('.hero').getBoundingClientRect()
+let landingTopWidth = landingTop.width
+let landingTopHeight = landingTop.height
+let landingBookopusOffset = document.querySelector('#works1').getBoundingClientRect().top
+
 window.addEventListener("scroll", () => {
     let cardItem = Array.from(document.querySelectorAll('.cardimg__item'))
     let body = document.querySelector('body')
@@ -74,15 +81,15 @@ window.addEventListener("scroll", () => {
     let logoOuter = document.querySelector('.logo svg .d')
     let links = Array.from(document.querySelectorAll('.nav ul li a'))
 
-    if(window.pageYOffset * 3.1 > canvas.height){
+    if(window.pageYOffset + 400 > landingTopHeight){
         for(let i = 0; i < cardItem.length; i++){
             cardItem[i].classList.add('cardimg--reveal')
         }
     }
     function checkWorks(){
-        if(window.pageYOffset + 50 > canvas.height){
+        if(window.pageYOffset + 100 > landingTopHeight){
             body.style.backgroundColor = "var(--bookopus)"
-            if (canvas.width > 900){
+            if (landingTopWidth > 900){
                 logoInner.style.fill = "var(--light)"
                 logoOuter.style.stroke = "var(--dark)"
                 for(let i = 0; i < links.length; i++){
@@ -91,7 +98,7 @@ window.addEventListener("scroll", () => {
             }
         }    else{ /* Default Values */
             body.style.backgroundColor = "var(--light)"
-            if (canvas.width > 900){
+            if (landingTopWidth > 900){
                 logoInner.style.fill = "var(--cta)"
                 logoOuter.style.stroke = "var(--dark)"
                 for(let i = 0; i < links.length; i++){
@@ -101,16 +108,16 @@ window.addEventListener("scroll", () => {
         }
     }
     checkWorks();
-    if(window.pageYOffset + contact.scrollHeight - 450  > canvas.height +  contact.scrollHeight ){
+    if(window.pageYOffset + contact.scrollHeight - 450  > landingTopHeight +  contact.scrollHeight ){
         body.style.backgroundColor = "var(--dark)"
-        if (canvas.width > 900){
+        if (landingTopWidth > 900){
             logoInner.style.fill = "var(--cta)"
             logoOuter.style.stroke = "var(--light)"
             for(let i = 0; i < links.length; i++){
                 links[i].style.color = "var(--light)"
             }
         }
-    }else if (window.pageYOffset + 200 > canvas.height || window.pageYOffset + contact.scrollHeight - 900  < canvas.height +  contact.scrollHeight){
+    }else if (window.pageYOffset + 200 > landingTopHeight || window.pageYOffset + contact.scrollHeight - 900  < landingTopHeight +  contact.scrollHeight){
         checkWorks();
     }
 })
